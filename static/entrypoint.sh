f21de3120fe920247892db6d3b8740bacdcf4439
botnet <root@botnet.me>
2021-03-29 17:29:35 +0200

    V1.2

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 set -e
+
+echo -e "\t\tYour pathfinder is up! Visit it at https://$DOMAIN"
+cat << "EOF"
+
+                           .       .
+                          / `.   .' \
+                  .---.  <    > <    >  .---.
+                  |    \  \ - ~ ~ - /  /    |
+                   ~-..-~             ~-..-~
+               \~~~\.'                    `./~~~/
+     .-~~^-.    \__/                        \__/
+   .'  O    \     /               /       \  \
+  (_____,    `._.'               |         }  \/~~~/
+   `----.          /       }     |        /    \__/
+         `-.      |       /      |       /      `. ,~~|
+             ~-.__|      /_ - ~ ^|      /- _      `..-'   f: f:
+                  |     /        |     /     ~-.     `-. _||_||_
+                  |_____|        |_____|         ~ - . _ _ _ _ _>
+                    (c) Stegosaurus by Michael John Wagoner
+                                                         
+EOF
+printf "${NC}"
+
 envsubst '$DOMAIN' </etc/nginx/sites_enabled/templateSite.conf >/etc/nginx/sites_enabled/site.conf
-envsubst '$CONTAINER_NAME' </etc/nginx/templateNginx.conf >/etc/nginx/nginx.conf
 envsubst  </var/www/html/pathfinder/app/templateEnvironment.ini >/var/www/html/pathfinder/app/environment.ini
 envsubst  </var/www/html/pathfinder/app/templateConfig.ini >/var/www/html/pathfinder/app/config.ini
 envsubst  </etc/zzz_custom.ini >/etc/php7/conf.d/zzz_custom.ini
 htpasswd   -c -b -B  /etc/nginx/.setup_pass pf "$APP_PASSWORD"
-crontab /var/crontab.txt
+crontab  /var/crontab.txt
 exec "$@"
